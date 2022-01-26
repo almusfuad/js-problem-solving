abb973a64d021f5e07fd074b20774c324926ae94
AlmusFuad <almusfuad@gmail.com>
2022-01-27 02:19:19 +0600

    finding type

---
+++ converting-type.js
@@ -0,0 +1,5 @@
+var stringInt = "42";
+var stringFloat = "40.5";
+
+console.log(parseInt(stringInt));
+console.log(parseFloat(stringFloat));
---
+++ finding-type.js
@@ -0,0 +1,7 @@
+var name = "fuad";
+var age = 28;
+var money = 11.5;
+
+console.log('This is the name variable type: ', typeof(name));
+console.log("This is the age variable type: ", typeof(age));
+console.log('This is the money variable type: ', typeof(money));
