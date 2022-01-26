919e9e5618e282d067eb4f2878265124923a40e2
AlmusFuad <almusfuad@gmail.com>
2022-01-27 02:25:24 +0600

    fixed after decimal

---
+++ fixed-after-decimal.js
@@ -0,0 +1,6 @@
+var float = 12.1234567;
+
+console.log('This is the type of: ', typeof(float));
+console.log('This is fixed 1 after decimal: ', float.toFixed(1));
+console.log('This is fixed 2 after decimal: ', float.toFixed(2));
+console.log('This is fixed 3 after decimal: ', float.toFixed(3));
