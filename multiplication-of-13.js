1667e5201af9b80e9775fd1b1a527050e9f86f66
AlmusFuad <almusfuad@gmail.com>
2022-02-01 21:28:50 +0600

    alphabet case change

--- multiplication-of-13.js
+++ multiplication-of-13.js
@@ -1,6 +1,10 @@
+// declare a number 
 let number = 1;
 
+
+// use fuction to do whole operation 
 function multiplication(number) {
+    // use loop to continue the process 
     for (let i = 1; i <= 10; i++) {
         var result = number * i;
 
---
+++ uppercase-to-lowercase.js
@@ -0,0 +1,13 @@
+// declare a text in lowercase 
+
+let lowerText = 'this is lowercase text';
+// convert lowertext to uppertext 
+let toUpperText = lowerText.toUpperCase();
+console.log('Your lowertext: ', lowerText, '\n', 'Lowertext to uppertext: ', toUpperText);
+
+
+// declare a uppercase text 
+let upperText = 'THIS IS UPPERCASE TEXT.'
+    // convert upperText to lowerText 
+let toLowerText = upperText.toLowerCase();
+console.log('Your uppercase: ', upperText, '\n', 'Uppertext to lowertext: ', toLowerText);
