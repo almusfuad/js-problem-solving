e81b45832c64efbd0b607beb0a54710c1eb548ce
AlmusFuad <almusfuad@gmail.com>
2022-02-01 20:56:50 +0600

    multiplication using for loop

---
+++ multiplication-of-13.js
@@ -0,0 +1,12 @@
+let number = 1;
+
+function multiplication(number) {
+    for (let i = 1; i <= 10; i++) {
+        var result = number * i;
+
+        console.log(number, '*', i, '=', result);
+    }
+
+}
+
+var newVal = multiplication(13);
