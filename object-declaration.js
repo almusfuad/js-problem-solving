25e02eda3c6150333540a8895114e2c2ceb8934e
AlmusFuad <almusfuad@gmail.com>
2022-02-01 20:33:25 +0600

    result sheet

---
+++ object-declaration.js
@@ -0,0 +1,7 @@
+// object declaration 
+const objectName = {
+    arrayProperty: ['values1', 'values2', 'values3', '...'],
+    normalProperty1: 'values',
+    normalProperty2: 'values',
+    normalProperty3: 'Values',
+}
---
+++ result-sheet.js
@@ -0,0 +1,15 @@
+let marks = 91;
+
+if (marks < 50) {
+    console.log('You get F');
+} else if (marks < 60) {
+    console.log('You get D');
+} else if (marks < 70) {
+    console.log('YOu get C');
+} else if (marks < 80) {
+    console.log('You get B');
+} else if (marks < 90) {
+    console.log('You get A');
+} else {
+    console.log('You get A+');
+}
