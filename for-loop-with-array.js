5ea5b4006410646bef7ef22d146a161e8156abf2
AlmusFuad <almusfuad@gmail.com>
2022-02-01 16:46:07 +0600

    array-values

---
+++ for-loop-with-array.js
@@ -0,0 +1,17 @@
+// declare an array 
+var arrayName = [45, 86, 34, 87, 12];
+
+// all elements value 
+for (var i = 0; i < arrayName.length; i++) {
+    var element = arrayName[i];
+    console.log(element);
+}
+
+
+// object declaration 
+const objectName = {
+    arrayProperty: ['values1', 'values2', 'values3', '...'],
+    normalProperty1: 'values',
+    normalProperty2: 'values',
+    normalProperty3: 'Values',
+}
