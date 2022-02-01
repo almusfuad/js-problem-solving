4c24f20912a64aa01506e20fef621d3a8d63799b
AlmusFuad <almusfuad@gmail.com>
2022-02-01 16:49:37 +0600

    for loop with array

--- for-loop-with-array.js
+++ for-loop-with-array.js
@@ -6,12 +6,3 @@ for (var i = 0; i < arrayName.length; i++) {
     var element = arrayName[i];
     console.log(element);
 }
-
-
-// object declaration 
-const objectName = {
-    arrayProperty: ['values1', 'values2', 'values3', '...'],
-    normalProperty1: 'values',
-    normalProperty2: 'values',
-    normalProperty3: 'Values',
-}
