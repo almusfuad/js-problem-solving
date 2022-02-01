b5b3fecf5051f1fd716a0ffe507de5585cf72b62
AlmusFuad <almusfuad@gmail.com>
2022-02-01 21:44:14 +0600

    object data updating

--- object-declaration.js
+++ object-declaration.js
@@ -5,3 +5,21 @@ const objectName = {
     normalProperty2: 'values',
     normalProperty3: 'Values',
 }
+
+// get object value 
+var objectValue = objectName.arrayProperty;
+console.log(objectValue);
+
+// set object value by property
+objectName.normalProperty1 = "newValue";
+console.log('Objectvalue: ', objectName);
+
+
+// add value by specified 
+objectName['values'] = "newValue";
+console.log('new value in property list: ', objectName);
+
+
+// set value in different way 
+objectName["normalProperty3"] = 'newValues';
+console.log('new value in property2: ', objectName);
